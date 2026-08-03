4e81104bf438158f3d0cc6dca746673d775923b2
박현호 <hyunho@ati2000.co.kr>
2026-08-03 14:55:48 +0900

    feat: Ch03_P18 추가
    Program.cs에서 HAL 9000 인용문을 출력하도록 구현

---
+++ Ch03_P18/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch03_P18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I’m sorry, Dave. I’m afraid I can’t do that.");
+        }
+    }
+}
