6fec86cb1a0d886d74d136667e6c2eac228120a3
박현호 <hyunho@ati2000.co.kr>
2026-08-03 15:38:46 +0900

    Ch05_P28 추가

---
+++ Ch05_P28/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05_P28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            b = a;
+            a = -3;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}
