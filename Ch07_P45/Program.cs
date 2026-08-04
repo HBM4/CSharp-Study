f5d26c70f69f080eb60ac2d9797573ffacb6fc63
박현호 <hyunho@ati2000.co.kr>
2026-08-04 09:29:53 +0900

    Ch07 P45 추가

---
+++ Ch07_P45/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ch07_P45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17, b = 4;
+            int quotient, remainder;
+            
+            quotient = a / b;
+            remainder = a % b;
+
+            Console.WriteLine($"Quotient: {quotient}, Remainder: {remainder}");
+
+            int newVar = b * quotient + remainder;
+            Console.WriteLine(a == newVar ? "The equation holds true." : "The equation does not hold true.");
+        }
+    }
+}
