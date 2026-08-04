7aeb4b6567f8252b37e1fa67ea55b97831cc510a
박현호 <hyunho@ati2000.co.kr>
2026-08-04 09:20:11 +0900

    Ch07 P44 추가

---
+++ Ch07_P44/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ch07_P44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = 5;
+            double h = 6;
+            double A = (b * h) / 2;
+
+            Console.WriteLine($"Area: {A:f2}");
+        }
+    }
+}
