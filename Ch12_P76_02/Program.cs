d844dd6640104219a6fdf268f582c6bf45e401e9
박현호 <hyunho@ati2000.co.kr>
2026-08-04 16:54:53 +0900

    Ch12 P76 02번 추가

---
+++ Ch12_P76_02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ch12_P76_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // FizzBuzz 문제
+
+            for(int i=1; i<=100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
