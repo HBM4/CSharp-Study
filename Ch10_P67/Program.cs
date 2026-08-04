a231406e96a64326091ae55ce77507b695b7014d
박현호 <hyunho@ati2000.co.kr>
2026-08-04 15:00:02 +0900

    Ch10 P67 개선

--- Ch10_P67/Program.cs
+++ Ch10_P67/Program.cs
@@ -11,9 +11,8 @@
             Console.WriteLine("Enter second number:");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            // 출력: 곱셈 결과가 양수인지 음수인지 여부
-            // 둘다 양수이거나, 둘다 음수일 경우 양수이다.
-            bool isPositive = (num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0);
+            // 곱셈 결과가 양수이면 true (둘 다 양수 또는 둘 다 음수)
+            bool isPositive = num1 * num2 > 0;
             Console.WriteLine(isPositive);
         }
     }
