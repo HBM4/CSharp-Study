6d2febb9ad404e3df20f2dde9ba188018605ed95
박현호 <hyunho@ati2000.co.kr>
2026-08-04 15:31:49 +0900

    Ch11 P70 추가

---
+++ Ch11_P70/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ch11_P70
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator in English (+, -, *, /, %):");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine($"{a} + {b} = {a + b:f2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{a} - {b} = {a - b:f2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{a} * {b} = {a * b:f2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{a} / {b} = {a / b:f2}");
+                    break;
+                case "%":
+                    Console.WriteLine($"{a} % {b} = {a % b:f2}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
