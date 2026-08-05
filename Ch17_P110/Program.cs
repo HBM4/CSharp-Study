5e55a9d3f50f7f747848c9279414dafd45652caa
박현호 <hyunho@ati2000.co.kr>
2026-08-05 15:35:07 +0900

    Ch17 P110 추가

---
+++ Ch17_P110/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ch17_P110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (n > 0)
+            {
+                sum += RandomNumber();
+                n--;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        static int RandomNumber()
+        {
+            Random random = new Random();
+
+            return random.Next(6) + 1;
+        }
+    }
+}
