ba23eaff13466eb39ec972231bb579508d22e0de
박현호 <hyunho@ati2000.co.kr>
2026-08-05 12:59:21 +0900

    Ch15 P96 추가

---
+++ Ch15_P96/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ch15_P96
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 숫자를 입력 받으면 피보나치 수열 n번째 값 출력
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(FibonacciSequence(n));
+        }
+
+        static int FibonacciSequence(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return FibonacciSequence(number - 1) + FibonacciSequence(number - 2);
+            }
+        }
+    }
+}
