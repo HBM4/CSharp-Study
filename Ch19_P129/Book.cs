1832136dd1d434d0e7d90b849844acd812cdd389
박현호 <hyunho@ati2000.co.kr>
2026-08-07 17:17:55 +0900

    feat: Ch19_P129 예제 프로젝트 추가
    C# 기초 교재 Ch19 P129의 도서 관리 예제 프로젝트를 신규 추가
    
    - Book.cs: 자동 구현 및 읽기 전용 프로퍼티, 단어 수 계산 로직을 가진 Book 클래스 구현
    - Program.cs: Book 인스턴스를 생성하여 데이터를 할당하고 정보를 출력하는 진입점 추가

---
+++ Ch19_P129/Book.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch19_P129
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        // 자동 구현 프로퍼티
+        public string Publisher { get; set; }
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value < 0)
+                    pages = 0;
+                else
+                    pages = value;
+            }
+        }
+
+        public int WordCount
+        {
+            get
+            {
+                return wordCount;
+            }
+            // set을 작성하지 않으면 WordCount는 읽기 전용이 됩니다.
+        }
+
+        // WordCount를 계산하는 메서드
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
---
+++ Ch19_P129/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ch19_P129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Harry Potter", "J.K. Rowling");
+
+            book.Title = "Harry Potter and Half-Blood Prince.";
+            book.Pages = 607;
+            book.Publisher = "Bloomsbury";
+
+            book.AssignWordCountFromText("This is a sample text to count the number of words in the book.");
+
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+            Console.WriteLine(book.Pages);
+            Console.WriteLine(book.WordCount);
+            Console.WriteLine(book.Publisher);
+        }
+    }
+}
