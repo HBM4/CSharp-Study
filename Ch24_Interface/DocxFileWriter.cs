b2c3eab9d1ca94b038097e3e8bd4f13236e2aad0
박현호 <hyunho@ati2000.co.kr>
2026-08-10 12:16:27 +0900

    Ch24 Interface 추가

---
+++ Ch24_Interface/DocxFileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch24_Interface
+{
+    internal class DocxFileWriter : IFileWriter
+    {
+        public string Extension
+        {
+            get { return ".docx"; }
+        }
+
+        public void Write(string filename)
+        {
+            // 여기에 DOCX 파일 작성 코드를 작성합니다.
+            Console.WriteLine($"Writing to {filename}");
+        }
+    }
+}
