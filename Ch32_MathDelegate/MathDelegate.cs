f2fdbcc6968f00be3881184ff19dd4f3fe864338
박현호 <hyunho@ati2000.co.kr>
2026-08-11 08:54:11 +0900

    Ch32 MathDelegate 추가

---
+++ Ch32_MathDelegate/MathDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch32_MathDelegate
+{
+    // delegate 타입 정의도 이곳에 두는 것이 자연스럽습니다.
+    public delegate int MathDelegate(int a, int b);
+
+    public static class MathOperations
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Power(int baseNumber, int exponent)
+        {
+            return (int)Math.Pow(baseNumber, exponent);
+        }
+    }
+}
