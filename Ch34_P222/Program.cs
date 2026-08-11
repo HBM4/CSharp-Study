5ae4efcc9be78658516babac7886054b4553f366
박현호 <hyunho@ati2000.co.kr>
2026-08-11 14:37:59 +0900

    Ch34 P222 추가

---
+++ Ch34_P222/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ch34_P222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1, 2, 3);
+            Vector v2 = new Vector(3, 3, 3);
+
+            Vector v3 = v1 + v2;
+            Console.WriteLine($"v1 + v2 = ({v3.X}, {v3.Y}, {v3.Z})"); // (4, 5, 6)
+
+            Vector v4 = v1 - v2;
+            Console.WriteLine($"v1 - v2 = ({v4.X}, {v4.Y}, {v4.Z})"); // (-2, -1, 0)
+
+            Vector v5 = -v1;
+            Console.WriteLine($"-v1 = ({v5.X}, {v5.Y}, {v5.Z})"); // (-1, -2, -3)
+
+            Vector v6 = v1 * 4;
+            Console.WriteLine($"v1 * 4 = ({v6.X}, {v6.Y}, {v6.Z})"); // (4, 8, 12)
+
+            Vector v7 = v2 / 2;
+            Console.WriteLine($"v2 / 2 = ({v7.X}, {v7.Y}, {v7.Z})"); // (1.5, 1.5, 1.5)
+        }
+    }
+}
---
+++ Ch34_P222/Vector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch34_P222
+{
+    internal class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z {  get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        // 모든 연산자 오버로드는 public이면서, static이어야 한다.
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.X, -v.Y, -v.Z);
+        }
+
+        // 벡터에 숫자(스칼라)를 곱합니다. 그러면 (1, 2, 3) * 4는 (4, 8, 12)가 되어야 합니다.
+        public static Vector operator *(Vector v, double scalar)
+        {
+            return new Vector(v.X * scalar, v.Y * scalar, v.Z * scalar);
+        }
+
+        // 벡터를 숫자(스칼라)로 나눕니다. 그러면 (2, 4, 6) / 2는 (1, 2, 3)이 되어야 합니다.
+        public static Vector operator /(Vector v, double scalar)
+        {
+            return new Vector(v.X / scalar, v.Y / scalar, v.Z / scalar);
+        }
+    }
+}
