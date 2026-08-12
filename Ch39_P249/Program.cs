dd5c279ae1bfb6150da57f031448f722725d5ec0
박현호 <hyunho@ati2000.co.kr>
2026-08-12 14:35:49 +0900

    Ch39 P249 추가

---
+++ Ch39_P249/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ch39_P249
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(FrogRace);
+            Thread thread2 = new Thread(FrogRace);
+            Thread thread3 = new Thread(FrogRace);
+
+            thread1.Start(1);
+            thread2.Start(2);
+            thread3.Start(3);
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+        }
+
+        public static void FrogRace(object input)
+        {
+            int frogNumber = (int)input;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Frog #{frogNumber} jumped");
+                Thread.Sleep(Random.Shared.Next(0, 1001)); // Shared 의미: Random 객체를 여러 스레드에서 안전하게 공유할 수 있도록 제공하는 정적 속성
+            }
+
+            Console.WriteLine($"Frog #{frogNumber} finished");
+        }
+    }
+}
